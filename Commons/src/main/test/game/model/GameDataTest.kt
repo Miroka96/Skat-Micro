7b0747f1e65d35bd3c7ce713fa7e40875d4bf33e
mirko.krause <mirko.krause@student.hpi.de>
2017-08-20 10:53:17 +0200

    fulfilled jboss maven dependency

--- Commons/src/main/test/game/model/GameDataTest.kt
+++ Commons/src/main/test/game/model/GameDataTest.kt
@@ -1,5 +1,7 @@
 package game.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.jboss.arquillian.container.test.api.Deployment
 import org.jboss.arquillian.junit.Arquillian
 import org.jboss.shrinkwrap.api.ShrinkWrap
@@ -10,10 +12,28 @@ import org.junit.runner.RunWith
 @RunWith(Arquillian::class)
 class GameDataTest {
 
-
     @org.junit.Test
     fun toJson() {
         var gameData = GameData()
+
+        val mapper = jacksonObjectMapper()
+        val writer = mapper.writerWithDefaultPrettyPrinter()
+
+        val json1 = writer.writeValueAsString(Data1(1, "Foo", "Bar"))
+        val data1_1 = mapper.readValue(json1, Data1::class.java)
+        val data2_1 = mapper.readValue(json1, Data2::class.java)
+        println(json1)
+        println()
+        println(data1_1)
+        println(data2_1)
+        println()
+        val json2 = writer.writeValueAsString(Data2(1, "Baz", "Qux"))
+        val data1_2 = mapper.readValue(json2, Data1::class.java)
+        val data2_2 = mapper.readValue(json2, Data2::class.java)
+        println(json2)
+        println()
+        println(data1_2)
+        println(data2_2)
     }
 
     companion object {
@@ -27,3 +47,11 @@ class GameDataTest {
     }
 
 }
+
+data class Data1(val id: Int,
+                 val first: String,
+                 val last: String)
+
+data class Data2(@JsonProperty("id") val a: Int,
+                 @JsonProperty("first") val b: String,
+                 @JsonProperty("last") val c: String)
