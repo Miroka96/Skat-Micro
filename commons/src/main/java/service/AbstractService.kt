646c02c35e742530e0271c1451061047dccda623
mirko.krause <mirko.krause@student.hpi.de>
2017-09-22 23:11:12 +0200

    extracted JWT AuthProvider Creation

--- commons/src/main/java/service/AbstractService.kt
+++ commons/src/main/java/service/AbstractService.kt
@@ -1,24 +1,20 @@
 package service
 
 import io.vertx.core.*
-import io.vertx.core.buffer.Buffer
 import io.vertx.core.json.JsonObject
-import io.vertx.ext.auth.User
 import io.vertx.ext.auth.jwt.JWTAuth
 import io.vertx.ext.auth.jwt.JWTOptions
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.client.HttpResponse
-import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.handler.BodyHandler
 import service.database.AbstractQueries
 import service.database.CouchbaseAccess
+import service.jwt.AuthProviderManager
 import service.jwt.KeyStoreManager
 import service.request.AbstractRequestHandler
 import service.request.RequestHandlerWrapper
 import java.nio.file.NoSuchFileException
 import java.security.UnrecoverableKeyException
-import java.util.*
 
 
 abstract class AbstractService : AbstractVerticle() {
@@ -133,104 +129,31 @@ abstract class AbstractService : AbstractVerticle() {
 
     private fun initializeAuthProvider(continueFuture: Future<Unit>) {
         vertx.executeBlocking<Unit>({ future ->
+            val authProviderManager = AuthProviderManager(vertx, conf)
             try {
-                if (createKeyStorePermission) {
-                    authProvider = keyStoreManager.getJWTAuthProvider()
-                } else {
-                    authProvider = keyStoreManager.getJWTAuthProviderByRead()
-                }
-                checkAuthProvider(future)
+                authProvider = authProviderManager.readJWTAuthProvider(keyStoreManager, createKeyStorePermission)
+                authProviderManager.testTokenCreation(authProvider, tokenOptions)
             } catch (keyEx: UnrecoverableKeyException) {
                 future.fail(keyEx)
             } catch (nsfEx: NoSuchFileException) {
                 println("Pulling Public Key")
-                val providerFuture = Future.future<JWTAuth>()
-                providerFuture.setHandler { res: AsyncResult<JWTAuth> ->
-                    if (res.succeeded()) {
-                        authProvider = res.result()
-                        future.complete()
-                    } else {
-                        future.fail(res.cause())
-                    }
+                try {
+                    authProvider = authProviderManager.pullReadOnlyJWTAuthProvider()
+                } catch (ex: Exception) {
+                    println("Exception while pulling public key")
+                    ex.printStackTrace()
+                    future.fail(ex)
                 }
-                pullReadOnlyAuthProvider(providerFuture)
             } catch (ex: Exception) {
                 println("Unknown Exception while reading Keystore")
                 future.fail(ex)
             }
+            future.complete()
         }, { res: AsyncResult<Unit> ->
             continueFuture.handle(res)
         })
     }
 
-    private fun pullReadOnlyAuthProvider(future: Future<JWTAuth>) {
-        val keyFuture = Future.future<String>()
-        keyFuture.setHandler { res: AsyncResult<String> ->
-            if (res.succeeded()) {
-                val publicKey = res.result()
-                val jwtConfig = JsonObject()
-                        .put(KeyStoreManager.PUBLIC_KEY, publicKey)
-
-                val authProvider = JWTAuth.create(vertx, jwtConfig)
-                future.complete(authProvider)
-            } else {
-                future.fail(res.cause())
-            }
-        }
-        downloadJWTPublicKey(keyFuture)
-    }
-
-    private fun downloadJWTPublicKey(keyFuture: Future<String>) {
-        val client = WebClient.create(vertx)
-        val request = client.get(
-                conf.getJsonObject(ServiceConfigInitializer.SERVICES)
-                        .getInteger(ServiceConfigInitializer.JWT_PORT),
-                conf.getJsonObject(ServiceConfigInitializer.SERVICES)
-                        .getString(ServiceConfigInitializer.JWT_HOST),
-                RoutingPath.PUBLIC_KEY.toString())
-
-        request.send { result: AsyncResult<HttpResponse<Buffer>> ->
-            if (result.succeeded()) {
-                val response = result.result()
-                val res = JsonObject(response.bodyAsString())
-                val pubKey = res.getString(KeyStoreManager.PUBLIC_KEY)
-                keyFuture.complete(pubKey)
-            } else {
-                keyFuture.fail(result.cause())
-            }
-        }
-    }
-
-    private fun checkAuthProvider(continueFuture: Future<Unit>) {
-        val token: String = authProvider.generateToken(
-                JsonObject().put("key", "test")
-                , tokenOptions
-        )
-        print("Sample Token: ")
-        println(token)
-
-        try {
-            val keyStore = keyStoreManager.loadKeyStore()
-            val publicKey = keyStoreManager.getPublicKey(keyStore, tokenOptions.algorithm)
-
-            print("Public Key: ")
-            println(Base64.getEncoder().encodeToString(publicKey.encoded))
-        } catch (ex: NullPointerException) {
-            println("No public key found for algorithm '${tokenOptions.algorithm}'")
-        }
-        println()
-
-        authProvider.authenticate(
-                JsonObject().put("jwt", token)
-        ) { res: AsyncResult<User> ->
-            if (res.succeeded()) {
-                continueFuture.complete()
-            } else {
-                println("JWT Test failed")
-                continueFuture.fail(res.cause())
-            }
-        }
-    }
 
     private fun checkCreateDatabaseIndex(continueFuture: Future<Unit>) {
         vertx.executeBlocking<Unit>({ future: Future<Unit> ->
--- commons/src/main/java/service/jwt/AuthProviderManager.kt
+++ commons/src/main/java/service/jwt/AuthProviderManager.kt
@@ -1,3 +1,86 @@
 package service.jwt
 
-class AuthProviderManager
+import io.vertx.core.AsyncResult
+import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.auth.User
+import io.vertx.ext.auth.jwt.JWTAuth
+import io.vertx.ext.auth.jwt.JWTOptions
+import io.vertx.ext.web.client.HttpResponse
+import io.vertx.ext.web.client.WebClient
+import service.RoutingPath
+import java.util.concurrent.CountDownLatch
+import service.ServiceConfigInitializer.Companion as Conf
+
+class AuthProviderManager(val vertx: Vertx, val config: JsonObject) {
+
+    fun readJWTAuthProvider(keyStoreManager: KeyStoreManager, createKeyStore: Boolean): JWTAuth {
+        return if (createKeyStore) {
+            keyStoreManager.getJWTAuthProvider()
+        } else {
+            keyStoreManager.getJWTAuthProviderByRead()
+        }
+    }
+
+    fun pullReadOnlyJWTAuthProvider(): JWTAuth {
+        val publicKey = downloadJWTPublicKey()
+        println("Got JWT public key:\n$publicKey")
+        val jwtConfig = JsonObject()
+                .put(KeyStoreManager.PUBLIC_KEY, publicKey)
+
+        val authProvider = JWTAuth.create(vertx, jwtConfig)
+        return authProvider
+    }
+
+    private fun downloadJWTPublicKey(): String {
+        val client = WebClient.create(vertx)
+        val request = client.get(
+                config.getJsonObject(Conf.SERVICES)
+                        .getInteger(Conf.JWT_PORT),
+                config.getJsonObject(Conf.SERVICES)
+                        .getString(Conf.JWT_HOST),
+                RoutingPath.PUBLIC_KEY.toString())
+
+        var publicKey: String = ""
+        val lock = CountDownLatch(1)
+        request.send { result: AsyncResult<HttpResponse<Buffer>> ->
+            if (result.succeeded()) {
+                val response = result.result()
+                val res = JsonObject(response.bodyAsString())
+                publicKey = res.getString(KeyStoreManager.PUBLIC_KEY)
+            } else {
+                throw result.cause()
+            }
+            lock.countDown()
+        }
+        lock.await()
+        return publicKey
+    }
+
+    fun testTokenCreation(authProvider: JWTAuth, tokenOptions: JWTOptions) {
+        println("Testing Token Creation")
+        val token: String = authProvider.generateToken(
+                JsonObject().put("key", "test"),
+                tokenOptions
+        )
+        print("Sample Token: ")
+        println(token)
+        println()
+
+        val lock = CountDownLatch(1)
+        authProvider.authenticate(
+                JsonObject().put("jwt", token)
+        ) { res: AsyncResult<User> ->
+            print("JWT Test ")
+            if (res.succeeded()) {
+                println("succeeded")
+            } else {
+                println("failed")
+                throw res.cause()
+            }
+            lock.countDown()
+        }
+        lock.await()
+    }
+}
